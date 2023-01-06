9a1359451586cde4f250a673a56f33d690ec27bd
ChandraShekharSaini <112823190+ChandraShekharSaini@users.noreply.github.com>
2023-01-07 00:06:11 +0530

    Add files via upload

---
+++ Q1.c
@@ -0,0 +1,19 @@
+//C program to perform all arithmetic operations
+//C program to perform all arithmetic operations
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    
+    printf("Enter number1:");
+    scanf("%d",&n);
+    printf("Enter number2:");
+    scanf("%d",&m);
+
+    printf("Addition of two numbers: %d\n",m+n);
+    printf("Subtraction of two number:%d\n",n-m);
+    printf("Multiplication of two number:%d\n",m*n);
+    printf("Division of two number:%d\n",n/m);
+    
+    return 0;
+}
